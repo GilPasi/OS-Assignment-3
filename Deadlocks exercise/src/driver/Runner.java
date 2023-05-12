c13cfe4c7a3371cd334e214a871c88d37ede0205
Gil_Pasi <95695418+GilPasi@users.noreply.github.com>
2023-05-12 14:02:44 +0300

    Laying infastracture

---
+++ Deadlocks exercise/src/driver/Runner.java
@@ -0,0 +1,15 @@
+/*Authors:
+ * Gil Pasi 	- 206500936
+ * Heba Abu-Kaf - 323980441
+ * */
+package driver;
+import implementation.*;
+public class Runner {
+	public static void main(String [] args) {
+		
+		System.out.println("Helssslo world");
+		Banker.foo();
+		
+	}
+
+}
---
+++ Deadlocks exercise/src/implementation/Banker.java
@@ -0,0 +1,12 @@
+/*Authors:
+ * Gil Pasi 	- 206500936
+ * Heba Abu-Kaf - 323980441
+ * */
+package implementation;
+public class Banker {
+	
+	public static void foo() {
+		System.out.println("Ahhhh!");
+	}
+
+}
